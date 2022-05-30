09353705250b54ced3d27e94c3809e92e057e915
Nelson Lai <nelsonlai9503@gmail.com>
2022-05-30 02:31:42 +0000

    add test

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## install solana
 
 ```sh
-sh -c "$(curl -sSfL https://release.solana.com/v1.9.1/install)"
+sh -c "$(curl -sSfL https://release.solana.com/v1.10.20/install)"
 ```
 
 add path:
@@ -23,3 +23,31 @@ npm i -g @project-serum/anchor-cli
 ```sh
 anchor init <your-project-name>
 ```
+
+## generate solana key
+
+```sh
+solana-keygen new --outfile ~/.config/solana/devnet.json
+solana config set --keypair ~/.config/solana/devnet.json
+```
+
+## set solana devnet
+
+```sh
+solana config set --url https://api.devnet.solana.com
+```
+
+## run test
+
+```sh
+anchor build
+anchor test
+```
+
+Error `error: no such subcommand: build-bpf`
+
+```sh
+rustup toolchain add 1.53
+```
+
+(you may need to update solana cli to latest version)
--- counter-program/tests/counter-program.ts
+++ counter-program/tests/counter-program.ts
@@ -1,16 +1,53 @@
 import * as anchor from "@project-serum/anchor";
+import { assert } from "chai";
 import { Program } from "@project-serum/anchor";
-import { CounterProgram } from "../target/types/counter_program";
+import { AnchorDemo } from "../target/types/anchor_demo";
 
-describe("counter-program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("anchor-demo", () => {
+    const provider = anchor.AnchorProvider.local();
+    anchor.setProvider(provider);
+    const counterAccount = anchor.web3.Keypair.generate();
+    const program = anchor.workspace.AnchorDemo as Program<AnchorDemo>;
 
-  const program = anchor.workspace.CounterProgram as Program<CounterProgram>;
+    it("should can be initialized", async () => {
+        const tx = await program.methods
+            .initialize()
+            .accounts({ counter: counterAccount.publicKey })
+            .signers([counterAccount])
+            .rpc();
+        console.log("Your transaction signature", tx);
+        assert.isTrue(
+            tx.length > 0,
+            "initialize() should return a transaction"
+        );
+    });
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    it("should can add value to count", async () => {
+        const tx = await program.methods
+            .add(new anchor.BN(3))
+            .accounts({ counter: counterAccount.publicKey })
+            .rpc();
+        console.log("Your transaction signature", tx);
+        assert.isTrue(tx.length > 0, "addOne() should return a transaction");
+
+        const count = await program.account.counter.fetch(
+            counterAccount.publicKey
+        );
+
+        assert.isTrue(count.count.toNumber() === 3, "counter should be 1");
+    });
+
+    it("should can minus value from count", async () => {
+        const tx = await program.methods
+            .minus(new anchor.BN(2))
+            .accounts({ counter: counterAccount.publicKey })
+            .rpc();
+        console.log("Your transaction signature", tx);
+        assert.isTrue(tx.length > 0, "minusOne() should return a transaction");
+
+        const count = await program.account.counter.fetch(
+            counterAccount.publicKey
+        );
+        assert.isTrue(count.count.toNumber() === 1, "counter should be 1");
+    });
 });
